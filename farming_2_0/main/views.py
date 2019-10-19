1be09ce4d829791b0d4643e2b75a6702de83e110
sampath <sampath.skt@gmail.com>
2019-10-19 21:04:08 +0530

    changed resp of machine

--- farming_2_0/main/views.py
+++ farming_2_0/main/views.py
@@ -5,17 +5,22 @@ def GetCrops(request):
     data = []
     for crop in list(Crop.objects.all()):
         data.append({"name": crop.name,
-                     "image":crop.image, "id":crop.cid})
+                     "image": crop.image, "id": crop.cid})
     return JsonResponse(data, content_type='application/json', safe=False)
 
-def GetStages(request,crop_id):
-    data = {}
+
+def GetStages(request, crop_id):
+    data = []
     for stage in list(Crop.objects.get(cid=crop_id).stages.all()):
-        data[stage.name] = {"image":stage.image,"sid":stage.sid}
-    return JsonResponse(data, content_type='application/json')
+        data.append({'name': stage.name,
+                     "image":stage.image,
+                     "id":stage.sid})
+    return JsonResponse(data, content_type='application/json', safe=False)
 
 def GetMachines(request,stage_id):
-    data = {}
+    data = []
     for machine in list(Stage.objects.get(sid=stage_id).machines.all()):
-        data[machine.name] = {"rent": machine.rent,"details":machine.details,"mid":machine.mid, "image":machine.image}
-    return JsonResponse(data, content_type='application/json')
+        data.append({"rent": machine.rent,"details":machine.details,"id":machine.mid, "image":machine.image,
+                     'name': machine.name})
+    return JsonResponse(data, content_type='application/json',
+                        safe=False)
